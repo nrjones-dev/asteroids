6936935fec26d457945ea0bf1875d0b6e31e0f32
nrjones-dev <nrjones.dev@gmail.com>
2025-05-28 20:38:25 +0100

    adds initial asteroids code

---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame
+
+import constants
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    game_running = True
+
+    while game_running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill(color="black")
+        pygame.display.flip()
+
+    print("Starting Asteroids!")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
